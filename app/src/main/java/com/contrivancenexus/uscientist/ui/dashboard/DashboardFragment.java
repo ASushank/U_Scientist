a53d48935310d2f65a85609d60ecfd0826bba466
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2023-10-08 16:06:57 +0545

    implemented dashboard

--- app/src/main/java/com/contrivancenexus/uscientist/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/contrivancenexus/uscientist/ui/dashboard/DashboardFragment.java
@@ -10,13 +10,32 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.contrivancenexus.uscientist.AddVideoActivity;
+import com.contrivancenexus.uscientist.DashboardVideoAdapter;
+import com.contrivancenexus.uscientist.Project;
+import com.contrivancenexus.uscientist.ProjectAdapter;
+import com.contrivancenexus.uscientist.VideoAct;
+import com.contrivancenexus.uscientist.VideoAdapter;
 import com.contrivancenexus.uscientist.databinding.FragmentDashboardBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    DatabaseReference databaseReference;
+    DashboardVideoAdapter videoAdapter;
+    ArrayList<VideoAct> list;
+    RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +43,6 @@ public class DashboardFragment extends Fragment {
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
         binding.addFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,7 +50,29 @@ public class DashboardFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        return root;
+        recyclerView = binding.videoDashRecyclerView;
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getUid()).child("UploadedVideos");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        list = new ArrayList<>();
+        videoAdapter = new DashboardVideoAdapter(getContext(), list);
+        recyclerView.setAdapter(videoAdapter);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    VideoAct video = dataSnapshot.getValue(VideoAct.class);
+                    list.add(video);
+                }
+                videoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return binding.getRoot();
     }
 
     @Override
